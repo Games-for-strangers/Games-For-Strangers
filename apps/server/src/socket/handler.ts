2a60b3df442013dd7883bf692c1ba76e3aced5bd
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-07-27 09:47:08 +0200

    server: connect gameState to socket handler

--- apps/server/src/socket/handler.ts
+++ apps/server/src/socket/handler.ts
@@ -2,6 +2,7 @@ import type { Server as SocketIOServer } from "socket.io";
 import type { Server as HttpServer } from "node:http";
 import { Server } from "socket.io";
 import { env } from "@gamesforstrangers/env/server";
+import { gameState } from "../state";
 import { RoundManager } from "./round-manager";
 
 export function createSocketServer(httpServer: HttpServer): SocketIOServer {
@@ -12,19 +13,27 @@ export function createSocketServer(httpServer: HttpServer): SocketIOServer {
     },
   });
 
+  gameState.setIO(io);
+
   const roundManager = new RoundManager(io);
   roundManager.init().catch(console.error);
 
   io.on("connection", (socket) => {
     console.log(`Socket connected: ${socket.id}`);
 
-    socket.on("join-game", ({ gameId, playerInfo }: { gameId: string; playerInfo: { animal: string; color: string } }) => {
-      roundManager.joinGame(socket, gameId, playerInfo);
-    });
+    socket.on(
+      "join-game",
+      ({ gameId, playerInfo }: { gameId: string; playerInfo: { animal: string; color: string } }) => {
+        roundManager.joinGame(socket, gameId, playerInfo);
+      },
+    );
 
-    socket.on("guess-submit", ({ gameId, roundId, guess }: { gameId: string; roundId: string; guess: string }) => {
-      roundManager.submitGuess(socket, gameId, roundId, guess);
-    });
+    socket.on(
+      "guess-submit",
+      ({ gameId, roundId, guess }: { gameId: string; roundId: string; guess: string }) => {
+        roundManager.submitGuess(socket, gameId, roundId, guess);
+      },
+    );
 
     socket.on("leave-game", ({ gameId }: { gameId: string }) => {
       roundManager.leaveGame(socket, gameId);
