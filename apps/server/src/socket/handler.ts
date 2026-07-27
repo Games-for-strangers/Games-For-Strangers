4acd1249d3d4ce8f99d5482eecf08afcf88d1930
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-07-27 09:33:48 +0200

    server: Socket.IO connection handler (join, guess, leave, disconnect)

---
+++ apps/server/src/socket/handler.ts
@@ -0,0 +1,40 @@
+import type { Server as SocketIOServer } from "socket.io";
+import type { Server as HttpServer } from "node:http";
+import { Server } from "socket.io";
+import { env } from "@gamesforstrangers/env/server";
+import { RoundManager } from "./round-manager";
+
+export function createSocketServer(httpServer: HttpServer): SocketIOServer {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST"],
+    },
+  });
+
+  const roundManager = new RoundManager(io);
+  roundManager.init().catch(console.error);
+
+  io.on("connection", (socket) => {
+    console.log(`Socket connected: ${socket.id}`);
+
+    socket.on("join-game", ({ gameId, playerInfo }: { gameId: string; playerInfo: { animal: string; color: string } }) => {
+      roundManager.joinGame(socket, gameId, playerInfo);
+    });
+
+    socket.on("guess-submit", ({ gameId, roundId, guess }: { gameId: string; roundId: string; guess: string }) => {
+      roundManager.submitGuess(socket, gameId, roundId, guess);
+    });
+
+    socket.on("leave-game", ({ gameId }: { gameId: string }) => {
+      roundManager.leaveGame(socket, gameId);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Socket disconnected: ${socket.id}`);
+      roundManager.disconnect(socket);
+    });
+  });
+
+  return io;
+}
